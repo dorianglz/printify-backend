7b2d5e87a061f4d2a16678d07516528cb40c4728
Dorian <dorian@mac.home>
2024-12-31 12:13:42 +0100

    change server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
